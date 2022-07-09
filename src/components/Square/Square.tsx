d9747faebb0a89991fc0fefc39aa13623d8b6535
Tuan Tran <langthamht047@gmail.com>
2022-07-08 18:30:27 -0700

    make an animation with framer motion

--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Square.css'
+import{ motion} from 'framer-motion'
 
 interface IProps {
     val: string,
@@ -8,10 +9,24 @@ interface IProps {
 const Square: React.FC<IProps> = (props) => {
 
     const {val,squareIndex} = props
+    const variants = {
+      filled: () =>({
+        scale: [1.2,1],
+        transition: {
+          duration: 0.2
+        }
+      }),
+      unfilled: () =>({
+        scale: [1.2,1],
+        transition: {
+          duration: 0.2
+        }
+      })
+    }
   return (
-    <>
+    <motion.div animate={val ? "filled" : "unfilled"} variants={variants}>
      <div className='square'>{val}</div>
-    </>
+    </motion.div>
   )
 }
 
